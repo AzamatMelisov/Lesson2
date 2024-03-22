71d65057320037857584b40f78306401b9b0bcca
AzamatMelisov <azamatmelisov5@gmail.com>
2024-03-22 19:06:36 +0600

    коментарий

---
+++ lesson2.dart
@@ -0,0 +1,73 @@
+// void main() {
+  // int a;
+  // int b = 10;
+  // print(b);
+
+  // double c = 20;
+  // print(c);
+  
+  // num d = 30;
+  // print(d);
+
+  // print(b+c);
+  // print(c+c);
+  // print(b+b);
+  // print(d+d);
+
+  // String name = ('Hello BIBY');
+  // print(name);
+  
+  // String name1 = ('салам алекум');
+  // print(name1);
+
+  // bool isMan = false;  //true или false
+  // print(isMan);
+  
+  // List array = [1,2,3,'b',true,false,b,c,d];
+  // print(array);
+
+  // List<String> studentsList = ['dastan','azamat','melis',];
+  // print(studentsList);
+  //  Map user = {'name':'dastan','age':10, 'isMan': true,};
+  //   print(user['name']);
+  // }
+  
+  // num, int, double, string, bool, list, map, final, const, dynamic, var).
+void main(){
+  num number1 = 10;
+  num number2 = 20;
+  num number3 = 30;
+  num number4 = 40;
+  num number5 = 50;
+
+
+  int a1 = 10;
+  int a2 = 20;
+  int a3 = 30;
+  int a4 = 40;
+  int a5 = 50;
+
+
+  double b1 = 10;
+  double b2 = 20;
+  double b3 = 30;
+  double b4 = 40;
+  double b5 = 50;
+
+
+  String name1 = ('azamat');
+  String familyName1 = ('Melisov');
+  String name2 =("dastan");
+  String familyName2 =('Temirbekov');
+  String name3 = ('beksultan');
+
+
+List<String> colleagues = ['dastan','azamat','melis'];
+List Group = ['Renat','Bolot','Belek'];
+List age =['23','32','26'];
+List height = ['180','185','190'];
+List carMake = ['TOYOTA','BMW','NISSAN'];
+
+  Map data = {'name':'azamat','age':22,};
+ 
+}
